173eb0d8aeac4987501e24b79756dcb96cc1dea1
Joao Pedro Romao <ojpzinho@gmail.com>
2021-01-18 21:01:14 -0300

    aprendendo sobre modulo file system do node

---
+++ script.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+/*
+  fs.writeFile('teste.txt', 'Hello World\r\n', error => {
+    if(error) {
+      throw error;
+    }
+
+    console.log("Arquivo criado com sucesso");
+  });
+*/
+
+/*
+  fs.appendFile('teste.txt', 'Hello World\r\n', error => {
+    if(error) {
+      throw error;
+    }
+
+    console.log("Adicionado ao arquivo com sucesso");
+  });
+*/
+
+/*
+  fs.unlink('teste.txt', error => {
+    if(error) {
+      throw error;
+    }
+
+    console.log("Arquivo deletado com sucesso!");
+  });
+*/
+
+/*
+fs.readFile('teste.txt', 'UTF8', (error, data) => {
+  if(error) {
+    throw error;
+  }
+
+  console.log(data);
+});
+*/
+
+/*
+  fs.rename('teste.txt', 'novoTeste.txt', error => {
+    if(error) {
+      throw error;
+    }
+
+    console.log("Arquivo renomeado com sucesso!");
+  });
+*/
